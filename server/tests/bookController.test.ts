0fc1b22e4376d2b469e8ccab7168bd8ee08372b4
CrysnaSony <krishnasoni1161@gmail.com>
2023-02-25 20:06:35 +0530

    test case fix

--- server/tests/bookController.test.ts
+++
@@ -1,8 +0,0 @@
-import bookController from "../controllers/bookController";
-
-test('get list of books', () => {
-    return bookController.list()
-              .then((data)=>{
-                  expect(data).toHaveProperty(['title'])
-              })
-  });
